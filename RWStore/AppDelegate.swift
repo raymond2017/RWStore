70e1be591417e50913065100f51300a1b6bc32bf
Yang <wistronits.cn@gmail.com>
2019-04-15 16:16:39 +0800

    Initial Commit

---
+++ RWStore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RWStore
+//
+//  Created by admin on 2019/4/15.
+//  Copyright © 2019 raymond. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RWStore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RWStore
+//
+//  Created by admin on 2019/4/15.
+//  Copyright © 2019 raymond. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
